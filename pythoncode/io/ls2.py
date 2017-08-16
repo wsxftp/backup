a417089212beeea13746de0bd1fc5b8052ad3f53
hunter <mortimer2015@hotmail.com>
2017-08-16 18:01:53 +0800

    ls2

--- pythoncode/io/ls2.py
+++ pythoncode/io/ls2.py
@@ -1,9 +1,15 @@
+# ecncoding: utf8
+# test
 import stat
 import argparse
 import pathlib
+import pwd
+import grp
+import datetime
 
 parser = argparse.ArgumentParser(prog='ls', description='use python make ls')
 parser.add_argument('-l', dest='long_format', help='-l 以长格式显示文件信息', action='store_true')
+parser.add_argument('-h', dest='human_read', help='-h 以易读的格式返回文件大小信息', action='store_true')
 parser.add_argument('path', nargs='*', default='.')
 args = parser.parse_args()
 
@@ -12,16 +18,32 @@ def scan(path):
     yield from (x for x in pathlib.Path(path).iterdir())
 
 
+def time_format(mtime):
+    dt = datetime.datetime.fromtimestamp(mtime)
+    return '{:>2} {:>2} {:>2}:{:>2}'.format(dt.month, dt.day, dt.hour, dt.minute)
+
+
+def human_format(mtime):
+    
+
+
 def format(iterm: pathlib.Path) -> str:
+    st = iterm.stat()
     if not args.long_format:
         return iterm
     else:
         ret = {
-            "mode": stat.filemode(iterm.stat().st_mode),
-            "size": iterm.stat().st_size,
+            "mode": stat.filemode(st.st_mode),
+            "nlink": st.st_link,
+            "user": pwd.getpwuid(st.st_uid).pw_name,
+            'group': grp.getgrgid(st.st_gid).gr_name,
+            "size": st.st_size,
+            "mtime": time_format(st.st_mtime),
             "name": iterm
         }
-    return '{mode} {size} {name}'.format(**ret)
+        if not args.human_read:
+            ret["size"] = 
+        return '{mode} {nlink} {user} {group} {size:>8} {mtime} {name}'.format(**ret)
 
 
 def main():
--- pythoncode/io/test.py
+++ pythoncode/io/test.py
@@ -1,11 +1,23 @@
 import stat
 import pathlib
+import datetime
 
 a = pathlib.Path('D:\logs').iterdir()
 for x in a:
     t = x.stat()
     mode = stat.filemode(t.st_mode)
     a = stat
-    print(mode)
+    print(t)
 
-help(stat)
+
+dt = datetime.datetime.fromtimestamp(t.st_mtime)
+print('{:>2} {:>2} {:>2}:{:>2}'.format(dt.month, dt.day, dt.hour, dt.minute))
+
+ret = {
+    "mode": 1,
+    "name": "bbb"
+}
+
+ret["mode"] = str(ret["mode"])
+
+print('{mode} {name}'.format(**ret))
